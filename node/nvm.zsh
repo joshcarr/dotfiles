07815ef6e42e834667086c799bf2bf76fca3f7a7
Josh Carr <josh@barbariangroup.com>
2012-03-30 16:27:46 -0700

    Remove undeeded sections. make the git prompt use my actual git. remove the hack for RVM since it seems to work now. add the NODEPATH

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -2,3 +2,10 @@
 . ~/.nvm/nvm.sh
 # use the default node version
 nvm use default
+
+NP=$(which node) 
+BP=${NP%bin/node} #this replaces the string '/bin/node'
+LP="${BP}lib/node_modules"
+
+export NODE_PATH="$LP"
+
