20b13b8d118ba26e4f14e0cb63e54d970af553d0
Josh Carr <josh.carr@aclimalabs.com>
2014-03-20 10:57:06 -0700

    abstracts time to function and styles

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -67,7 +67,11 @@ directory_name() {
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n%* $(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
+current_time() {
+  echo "%{$fg_bold[cyan]%}%*%{$reset_color%}"
+}
+
+export PROMPT=$'\n$(current_time) $(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
