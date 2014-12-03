e78284cc02b83e70b22aeae255eb04f505b090ca
Josh Carr <josh.carr@aclimalabs.com>
2014-12-03 13:31:54 -0800

    updates how we source the nvm stuff

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -1,11 +1,11 @@
-# source the Node Version Manager script
-. ~/.nvm/nvm.sh
-# use the default node version
-nvm use default
+# # source the Node Version Manager script
+# . ~/.nvm/nvm.sh
+# # use the default node version
+# nvm use default
 
-NP=$(which node) 
-BP=${NP%bin/node} #this replaces the string '/bin/node'
-LP="${BP}lib/node_modules"
+# NP=$(which node) 
+# BP=${NP%bin/node} #this replaces the string '/bin/node'
+# LP="${BP}lib/node_modules"
 
-export NODE_PATH="$LP"
+# export NODE_PATH="$LP"
 
